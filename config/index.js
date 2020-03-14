c3b1f5095f50d262f9a14178913489084b26b749
AnnatarHe <iamhele1994@gmail.com>
2020-03-14 20:46:21 +0800

    feat(taro): upgrade taro version to 2

--- config/index.js
+++ config/index.js
@@ -9,26 +9,24 @@ const config = {
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
-  plugins: {
-    babel: {
-      sourceMap: true,
-      presets: [
-        ['env', {
-          modules: false
-        }]
-      ],
-      plugins: [
-        'transform-decorators-legacy',
-        'transform-class-properties',
-        'transform-object-rest-spread',
-        ['transform-runtime', {
-          "helpers": false,
-          "polyfill": false,
-          "regenerator": true,
-          "moduleName": 'babel-runtime'
-        }]
-      ]
-    }
+  babel: {
+    sourceMap: true,
+    presets: [
+      ['env', {
+        modules: false
+      }]
+    ],
+    plugins: [
+      'transform-decorators-legacy',
+      'transform-class-properties',
+      'transform-object-rest-spread',
+      ['transform-runtime', {
+        "helpers": false,
+        "polyfill": false,
+        "regenerator": true,
+        "moduleName": 'babel-runtime'
+      }]
+    ]
   },
   defineConstants: {
   },
@@ -39,34 +37,30 @@ const config = {
     }
   },
   mini: {
-    module: {
-      postcss: {
-        pxtransform: {
-          enable: true,
-          config: {
-
-          }
-        },
-        url: {
-          enable: true,
-          config: {
-            limit: 10240 // 设定转换尺寸上限
-          }
-        },
-        cssModules: {
-          enable: true, // 默认为 false，如需使用 css modules 功能，则设为 true
-          config: {
-            namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]'
-          }
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {
         }
-      }
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 10240 // 设定转换尺寸上限
+        }
+      },
+      // cssModules: {
+      //   enable: true, // 默认为 false，如需使用 css modules 功能，则设为 true
+      //   config: {
+      //     namingPattern: 'module', // 转换模式，取值为 global/module
+      //     generateScopedName: '[name]__[local]___[hash:base64:5]'
+      //   }
+      // }
     }
   },
   h5: {
     publicPath: '/',
     staticDirectory: 'static',
-    module: {
       postcss: {
         autoprefixer: {
           enable: true,
@@ -85,7 +79,6 @@ const config = {
             generateScopedName: '[name]__[local]___[hash:base64:5]'
           }
         }
-      }
     }
   }
 }
