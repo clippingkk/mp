83f88dcd3e7d6c012a8d1e9e9882a661442c3ffa
AnnatarHe <iamhele1994@gmail.com>
2020-04-04 17:42:32 +0800

    feat(animation): add fade-in animation for item of list

--- src/pages/clipping/clipping.tsx
+++ src/pages/clipping/clipping.tsx
@@ -140,36 +140,39 @@ export default class Clipping extends Component<IClippingState> {
     // TODO: 折行处理
     const title = this.state.book.title
     return (
-      <View className='clipping-page'>
-        <View className='clipping-bg' />
-        <KKImage src={this.state.book.image} local-class='clipping-bg' />
-        <NavigationBar hasHolder onBack={this.back}>
-          <Text className='title'>{title}</Text>
-        </NavigationBar>
-        <View className='clipping-body'>
-          <View className='clipping-card'>
-            <Text className='title'>{this.state.clipping.title}</Text>
+      <View
+        className='clipping-page'
+        style={{ backgroundImage: `url(${this.state.book.image})`}}
+      >
+        <View className='clipping-bg'>
+          <NavigationBar hasHolder onBack={this.back}>
+            <Text className='title'>{title}</Text>
+          </NavigationBar>
+          <View className='clipping-body'>
+            <View className='clipping-card'>
+              <Text className='title'>{this.state.clipping.title}</Text>
 
-            <Text className='content'>
-              {this.state.clipping.content}
-            </Text>
+              <Text className='content'>
+                {this.state.clipping.content}
+              </Text>
 
-            <Text className='author'> —— {this.state.book.author}</Text>
+              <Text className='author'> —— {this.state.book.author}</Text>
+            </View>
+
+            <Button onClick={this.onSaveImage} className='btn-primary'>
+              🎨 保存
+            </Button>
           </View>
 
-          <Button onClick={this.onSaveImage} className='btn-primary'>
-            🎨 保存
-          </Button>
+          <Canvas
+            canvasId={canvasId}
+            className='out-canvas'
+            style={{
+              height: this.state.sysScreenSize.height + 'px',
+              width: this.state.sysScreenSize.width + 'px'
+            }}
+          />
         </View>
-
-        <Canvas
-          canvasId={canvasId}
-          className='out-canvas'
-          style={{
-            height: this.state.sysScreenSize.height + 'px',
-            width: this.state.sysScreenSize.width + 'px'
-          }}
-        />
       </View>
     )
   }
