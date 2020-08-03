2bd771a9e958cc5e14af43332dbcc8729dfe4f12
AnnatarHe <iamhele1994@gmail.com>
2020-08-03 22:35:51 +0800

    fix(api): move api to another

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,4 @@
 export const API_HOST = process.env.NODE_ENV === 'production' ?
-  'https://api.clippingkk.annatarhe.com/api' :
+  'https://clippingkk-api.annatarhe.com/api' :
   'http://192.168.0.109:9654/api'
 // export const API_HOST = 'https://api.clippingkk.annatarhe.com/api'
