9e1ea759accfbf8cc8b5058e1f2206343a0be624
AnnatarHe <iamhele1994@gmail.com>
2021-04-05 18:35:28 +0800

    fix(app): add promise.finally

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,13 @@
+import promiseFinally from 'promise.prototype.finally'
+promiseFinally.shim()
+
 import { ApolloProvider } from '@apollo/client';
 import * as React from 'react';
 import { Provider } from 'react-redux';
 import './app.css';
 import { client } from './services/ajax';
 import configStore from './store';
+
 
 const store = configStore()
 
