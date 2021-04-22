70eece05a17bb9981fe608a1d7eefe1e6117ea3c
AnnatarHe <iamhele1994@gmail.com>
2021-04-23 01:00:34 +0800

    fix(build): remove autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,19 @@
 // postcss.config.js
 module.exports = ({ options }) => {
+  // disable autoprefixer
+  const plugins = Object.keys(options.plugins)
+    .reduce((acc, k) => {
+      if (k.includes('postcss-preset-env')) {
+        acc[k] = { autoprefixer: false }
+      } else {
+        acc[k] = { k: options.plugins[k] }
+      }
+      return acc
+    }, {})
+
   return {
     plugins: {
-      ...options.plugins,
+      ...plugins,
     }
   }
 }
